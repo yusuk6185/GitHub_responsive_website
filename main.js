b924bd26ea0b19f2fdf019fb0b9162f52e897354
Ian Choi <yusuk6185@gmail.com>
2021-08-20 16:19:27 +0900

    project completed. worked on the responsiveness.

--- main.js
+++ main.js
@@ -8,9 +8,22 @@
         toggleElements();
     });
 
+    window.addEventListener('resize', function() {
+        if (window.innerWidth > 1024) {
+            // Off toggle element
+            offElements();
+        }
+    });
+
     function toggleElements() {
         [].forEach.call($toggles, function (toggle) {
             toggle.classList.toggle('on');
+        });
+    }
+
+    function offElements() {
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.remove('on');
         });
     }
 })(window, document)
